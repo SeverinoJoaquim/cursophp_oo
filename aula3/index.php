e3beede066bff3eb583ed5cef6812a8bdd0718c2
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-08 22:04:35 -0300

    aula 3 - get e set

---
+++ aula3/index.php
@@ -0,0 +1,43 @@
+<?php
+// Get e set
+
+class Login{
+	private $email;
+	private $senha;
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function setEmail($e) {
+		$email = filter_var($e, FILTER_SANITIZE_EMAIL);
+		$this->email = $email;
+	}
+
+	public function getSenha() {
+		return $this->senha;
+	}
+
+	public function setSenha($s) {
+		$this->senha = $s;
+	}
+
+	public function Logar() {
+		if($this->email == "teste@teste.com" and $this->senha == "123456"):
+			echo "Logado com sucesso!";
+		else:
+			echo "Dados inválidos!";
+		endif;
+	}
+}
+
+// Instanciar
+$logar = new Login();
+$logar->setEmail("teste@teste.com");
+$logar->setSenha("123456");
+$logar->Logar();
+echo "<br>";
+// Pergar elemento à tela
+echo $logar->getEmail();
+echo "<br>";
+echo $logar->getSenha();
