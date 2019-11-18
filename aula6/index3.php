7d1833587e4a7214a27ba089cf27fef89ddb1f43
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-17 21:37:30 -0300

    Aula 6

---
+++ aula6/index3.php
@@ -0,0 +1,47 @@
+<?php
+/*
+public - fará com que não haja ocultação nenhuma, toda propriedade ou método declarado com public serão acessíveis por todos que quiserem acessá-los.
+
+protected - visibilidade protected faz com que todos os herdeiros vejam as propriedades ou métodos protegidos como se fossem públicos.
+
+private - ao contrário do public, esse modificador faz com que qualquer método ou propriedade seja visível só e somente só pela classe que a declarou.
+*/
+
+class Veiculo {
+	protected $modelo;
+	public $cor;
+	public $ano;
+
+	public function Andar() {
+		echo "Andou";
+	}
+
+	public function Parar() {
+		echo "Parou";
+	}
+}
+
+class Carro extends Veiculo {
+	public function ligarLimpador() {
+		echo "Limpado em 54321";
+	}
+
+	public function setModelo($m) {
+		$this->modelo = $m;
+	}
+
+	public function getModelo() {
+		return $this->modelo;
+	}
+}
+
+class Moto extends Veiculo {
+	
+	public function darGrau() {
+		echo "Dando grau em 54321";
+	}		
+}
+
+$carro = new Carro();
+$carro->setModelo("HILUX");
+echo $carro->getModelo();
